6a3615a4e87b845b554ce9d1042540d52539f73a
aylierliu-hash <aylierliu@gmail.com>
2026-05-30 23:54:27 +0800

    Update StudentTest.java

--- day09-code/src/com/itheima/test2_5/StudentTest.java
+++ day09-code/src/com/itheima/test2_5/StudentTest.java
@@ -1,15 +1,33 @@
 package com.itheima.test2_5;
 
+/**
+ * 定义一个长度为3的数组，数组存储1~3名学生对象作为初始数据，学生对象的学号，姓名各不相同。
+ *
+ * 学生的属性：学号、姓名、年龄。
+ *
+ * 要求1：再次添加一个学生对象，并在添加的时候进行学号的唯一性判断。
+ *
+ * 要求2：添加完毕之后，遍历所有学生信息。
+ *
+ * 要求3：通过id删除学生信息
+ *
+ * 如果存在，则删除，如果不存在，则提示删除失败。
+ *
+ * 要求4：删除完毕之后，遍历所有学生信息。
+ *
+ * 要求5：查询数组id为“heima002”的学生，如果存在，则将他的年龄+1岁
+ */
+
 public class StudentTest {
     static void main(String[] args) {
-        Student[] students = new Student[3];
-
-        students[0] = new Student("1001", "张三", 18);
-        students[1] = new Student("1002", "李四", 19);
-        students[2] = new Student("1003", "王五", 20);
-
-        Student s1 = new Student("1001", "张三", 18);
-        Student s2 = new Student("1004", "李四", 19);
+//        Student[] students = new Student[3];
+//
+//        students[0] = new Student("1001", "张三", 18);
+//        students[1] = new Student("1002", "李四", 19);
+//        students[2] = new Student("1003", "王五", 20);
+//
+//        Student s1 = new Student("1001", "张三", 18);
+//        Student s2 = new Student("1004", "李四", 19);
 
         
 
